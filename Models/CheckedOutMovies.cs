6244f5b7c03e0319ba87245bdee5303738382802
asmelliem <asmelliem@gmail.com>
2020-10-28 22:00:50 -0400

    Created CheckOutMovies model

---
+++ Models/CheckedOutMovies.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab24_Bookstore.Models
+{
+    public class CheckedOutMovies
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public int MovieId { get; set; }
+        public DateTime DueDate { get; set; }
+    }
+}
