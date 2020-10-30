b279244bc298748353769788ea4e430c2bbe7467
asmelliem <asmelliem@gmail.com>
2020-10-29 21:52:36 -0400

    Added create method to MoveContoller.cs

--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Lab24_Bookstore.Data;
+using Lab24_Bookstore.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -27,6 +28,22 @@ namespace Lab24_Bookstore.Controllers
         {
             var movieList = await _context.Movie.ToListAsync();
             return View(movieList);
+        }
+        
+        // POST: Product/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Title,Genre,Runtime")] Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(movie);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(MovieList));
+            }
+            return View(movie);
         }
     }
 }
